30e50c45b29266816c10fa2b7c8087e641c90b29
petrjahoda <petrjahoda@protonmail.com>
2021-02-13 11:38:51 +0100

    Initial commit
    Took 1 hour 48 minutes

--- js/homepage.js
+++ js/homepage.js
@@ -4,31 +4,164 @@ d3.json("/get_timeline_data", {
         date: Date.now(),
     })
 }).then((data) => {
-    console.log(data)
-    DrawTimeLine(data["ProductionData"], data["DowntimeData"], data["PowerOffData"], data);
+    DrawTimeLine(data["ProductionData"], data["DowntimeData"]);
+    DrawTimeLinePanel(data["ProductionData"], data["DowntimeData"]);
 }).catch((error) => {
     console.error("Error loading the data: " + error);
 });
 
-function DrawTimeLine(productionDataset, downtimeDataset, powerOffDataset, data) {
-    // access data
+
+function DrawTimeLinePanel(productionDataset, downtimeDataset) {
+    const xAccessor = d => d["Date"]
+    const yAccessor = d => d["Value"]
+    let dimensionsPanel = {
+        width: screen.width - 500,
+        height: 40,
+        margin: {top: 0, right: 40, bottom: 20, left: 40,},
+    }
+
+    const wrapperPanel = d3.select("#timeline-panel")
+        .append("svg")
+        .attr("viewBox", "0 0 " + dimensionsPanel.width + " " + dimensionsPanel.height)
+    const boundsPanel = wrapperPanel.append("g")
+
+    const chartStartsAt = productionDataset[0]["Date"]
+    const chartEndsAt = productionDataset[productionDataset.length - 1]["Date"]
+
+    const xScalePanel = d3.scaleTime()
+        .domain([chartStartsAt, chartEndsAt])
+        .range([dimensionsPanel.margin.left, dimensionsPanel.width - dimensionsPanel.margin.right])
+    const yScalePanel = d3.scaleLinear()
+        .domain(d3.extent(productionDataset, yAccessor))
+        .range([dimensionsPanel.height - dimensionsPanel.margin.bottom, 0])
+
+    const productionAreaGeneratorPanel = d3.area()
+        .x(d => xScalePanel(xAccessor(d)))
+        .y0(dimensionsPanel.height - dimensionsPanel.margin.bottom)
+        .y1(d => yScalePanel(yAccessor(d)))
+        .curve(d3.curveStepAfter)
+    const downtimeAreaGeneratorPanel = d3.area()
+        .x(d => xScalePanel(xAccessor(d)))
+        .y0(dimensionsPanel.height - dimensionsPanel.margin.bottom)
+        .y1(d => yScalePanel(yAccessor(d)))
+        .curve(d3.curveStepAfter);
+
+    const timeScalePanel = d3.scaleTime()
+        .domain([new Date(chartStartsAt * 1000), new Date(chartEndsAt * 1000)])
+        .range([dimensionsPanel.margin.left, dimensionsPanel.width - dimensionsPanel.margin.right])
+    boundsPanel.append("g")
+        .attr("transform", "translate(0," + (dimensionsPanel.height - dimensionsPanel.margin.bottom) + ")")
+        .call(d3.axisBottom(timeScalePanel))
+
+    boundsPanel.append("path")
+        .attr("d", productionAreaGeneratorPanel(productionDataset))
+        .attr("fill", "green")
+    boundsPanel.append("path")
+        .attr("d", downtimeAreaGeneratorPanel(downtimeDataset))
+        .attr("fill", "orange")
+
+    const brush = d3.brushX()
+        .extent([[dimensionsPanel.margin.left, 0.5], [dimensionsPanel.width - dimensionsPanel.margin.right, dimensionsPanel.height - dimensionsPanel.margin.bottom + 0.5]])
+        .on("brush", updateData)
+        .on("end", updateData)
+
+    wrapperPanel.append("g")
+        .call(brush)
+
+    function updateData({selection}) {
+        if (selection) {
+            let startBrush = xScalePanel.invert(selection[0]) / 1000
+            let endBrush = xScalePanel.invert(selection[1]) / 1000
+            if (selection[0] > 0) {
+                document.getElementById("timeline").innerHTML = ""
+                let {productionDatasetUpdated, downtimeDatasetUpdated} = updateDatasets(productionDataset, startBrush, endBrush);
+                DrawTimeLine(productionDatasetUpdated, downtimeDatasetUpdated);
+            }
+        }
+
+    }
+}
+
+function updateDatasets(productionDataset, startBrush, endBrush) {
+    let productionDatasetUpdated
+    productionDatasetUpdated = []
+    let downtimeDatasetUpdated
+    downtimeDatasetUpdated = []
+    let startBrushing = true
+    let lastValueInBrush = 0
+    let lastValueBeforeBrush = 0
+    for (const actualElement of productionDataset) {
+        if (+(actualElement["Date"] / 1000) < +startBrush) {
+            lastValueBeforeBrush = actualElement["Value"]
+        } else if ((+(actualElement["Date"] / 1000) >= +startBrush) && (+(actualElement["Date"] / 1000) <= +endBrush)) {
+            if (startBrushing) {
+                console.log("First value from brush selection, inserting initial data into both datasets, based on actual value")
+                if (actualElement["Value"] === 1) {
+                    downtimeDatasetUpdated.push({Date: startBrush * 1000, Value: 1})
+                    productionDatasetUpdated.push({Date: startBrush * 1000, Value: 0})
+                    downtimeDatasetUpdated.push({Date: actualElement["Date"], Value: 0})
+                    productionDatasetUpdated.push({Date: actualElement["Date"], Value: 1})
+                } else {
+                    downtimeDatasetUpdated.push({Date: startBrush * 1000, Value: 0})
+                    productionDatasetUpdated.push({Date: startBrush * 1000, Value: 1})
+                    downtimeDatasetUpdated.push({Date: actualElement["Date"], Value: 1})
+                    productionDatasetUpdated.push({Date: actualElement["Date"], Value: 0})
+                }
+                startBrushing = false
+            } else {
+                console.log("Adding data to new datasets")
+                if (actualElement["Value"] === 1) {
+                    downtimeDatasetUpdated.push({Date: actualElement["Date"], Value: 0})
+                    productionDatasetUpdated.push({Date: actualElement["Date"], Value: 1})
+                } else {
+                    downtimeDatasetUpdated.push({Date: actualElement["Date"], Value: 1})
+                    productionDatasetUpdated.push({Date: actualElement["Date"], Value: 0})
+                }
+            }
+            lastValueInBrush = actualElement["Value"]
+        }
+    }
+    if (productionDatasetUpdated.length > 0) {
+        console.log("Closing new dataset with proper value, based on last value in brush selection")
+        if (lastValueInBrush === 1) {
+            downtimeDatasetUpdated.push({Date: endBrush * 1000, Value: 1})
+            productionDatasetUpdated.push({Date: endBrush * 1000, Value: 0})
+        } else {
+            downtimeDatasetUpdated.push({Date: endBrush * 1000, Value: 0})
+            productionDatasetUpdated.push({Date: endBrush * 1000, Value: 1})
+        }
+    } else {
+        console.log("No data in brush selection, inserting data based on last value before brush")
+        if (lastValueBeforeBrush === 1) {
+            productionDatasetUpdated.push({Date: startBrush * 1000, Value: 1})
+            productionDatasetUpdated.push({Date: endBrush * 1000, Value: 0})
+            downtimeDatasetUpdated.push({Date: startBrush * 1000, Value: 0})
+            downtimeDatasetUpdated.push({Date: endBrush * 1000, Value: 1})
+        } else {
+            productionDatasetUpdated.push({Date: startBrush * 1000, Value: 0})
+            productionDatasetUpdated.push({Date: endBrush * 1000, Value: 1})
+            downtimeDatasetUpdated.push({Date: startBrush * 1000, Value: 1})
+            downtimeDatasetUpdated.push({Date: endBrush * 1000, Value: 0})
+        }
+    }
+    return {productionDatasetUpdated, downtimeDatasetUpdated};
+}
+
+
+function DrawTimeLine(productionDataset, downtimeDataset) {
     const xAccessor = d => d["Date"]
     const yAccessor = d => d["Value"]
 
-    //set dimensions
     let dimensions = {
         width: screen.width - 500,
         height: 100,
         margin: {top: 0, right: 40, bottom: 20, left: 40,},
     }
-    //draw canvas
     const wrapper = d3.select("#timeline")
         .append("svg")
         .attr("viewBox", "0 0 " + dimensions.width + " " + dimensions.height)
     const bounds = wrapper.append("g")
 
-
-    //set scales
     const chartStartsAt = productionDataset[0]["Date"]
     const chartEndsAt = productionDataset[productionDataset.length - 1]["Date"]
     const xScale = d3.scaleTime()
@@ -38,8 +171,6 @@ function DrawTimeLine(productionDataset, downtimeDataset, powerOffDataset, data)
         .domain(d3.extent(productionDataset, yAccessor))
         .range([dimensions.height - dimensions.margin.bottom, 0])
 
-
-    // prepare data
     const productionAreaGenerator = d3.area()
         .x(d => xScale(xAccessor(d)))
         .y0(dimensions.height - dimensions.margin.bottom)
@@ -50,14 +181,8 @@ function DrawTimeLine(productionDataset, downtimeDataset, powerOffDataset, data)
         .y0(dimensions.height - dimensions.margin.bottom)
         .y1(d => yScale(yAccessor(d)))
         .curve(d3.curveStepAfter);
-    const powerOffAreaGenerator = d3.area()
-        .x(d => xScale(xAccessor(d)))
-        .y0(dimensions.height - dimensions.margin.bottom)
-        .y1(d => yScale(yAccessor(d)))
-        .curve(d3.curveStepAfter);
 
-    // prepare tooltip
-    let div = d3.select("#timeline").append("div")
+    d3.select("#timeline").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0.7)
         .style("visibility", "hidden");
@@ -68,63 +193,11 @@ function DrawTimeLine(productionDataset, downtimeDataset, powerOffDataset, data)
         .attr("transform", "translate(0," + (dimensions.height - dimensions.margin.bottom) + ")")
         .call(d3.axisBottom(timeScale))
 
-    // draw data with tooltip
     bounds.append("path")
         .attr("d", productionAreaGenerator(productionDataset))
         .attr("fill", "green")
-        .on('mousemove', (event) => {
-                let coords = d3.pointer(event);
-                let timeEntered = timeScale.invert(coords[0]) / 1000
-                let now = new Date(timeEntered * 1000).toLocaleString()
-                let start = new Date(productionDataset.filter(i => i["Date"] < timeEntered).pop()["Date"] * 1000).toLocaleString()
-                let end = new Date(productionDataset.filter(i => i["Date"] > timeEntered)[0]["Date"] * 1000).toLocaleString()
-                div.html(now + "<br/>Production<br/>" + start + "<br/>" + end)
-                    .style("visibility", "visible")
-                    .style("top", (event.pageY) - 60 + "px")
-                    .style("left", (event.pageX) - 60 + "px")
-            }
-        )
-        .on('mouseout', () => {
-                div.transition()
-                div.style("visibility", "hidden")
-            }
-        )
+
     bounds.append("path")
         .attr("d", downtimeAreaGenerator(downtimeDataset))
         .attr("fill", "orange")
-        .on('mousemove', (event) => {
-                let coords = d3.pointer(event);
-                let timeEntered = timeScale.invert(coords[0]) / 1000
-                let now = new Date(timeEntered * 1000).toLocaleString()
-                let start = new Date(downtimeDataset.filter(i => i["Date"] < timeEntered).pop()["Date"] * 1000).toLocaleString()
-                let end = new Date(downtimeDataset.filter(i => i["Date"] > timeEntered)[0]["Date"] * 1000).toLocaleString()
-                div.html(now + "<br/>Downtime<br/>" + start + "<br/>" + end)
-                    .style("visibility", "visible")
-                    .style("top", (event.pageY) - 60 + "px")
-                    .style("left", (event.pageX) - 60 + "px")
-            }
-        )
-        .on('mouseout', () => {
-                div.style("visibility", "hidden")
-            }
-        )
-    bounds.append("path")
-        .attr("d", powerOffAreaGenerator(powerOffDataset))
-        .attr("fill", "red")
-        .on('mousemove', (event) => {
-                let coords = d3.pointer(event);
-                let timeEntered = timeScale.invert(coords[0]) / 1000
-                let now = new Date(timeEntered * 1000).toLocaleString()
-                let start = new Date(powerOffDataset.filter(i => i["Date"] < timeEntered).pop()["Date"] * 1000).toLocaleString()
-                let end = new Date(powerOffDataset.filter(i => i["Date"] > timeEntered)[0]["Date"] * 1000).toLocaleString()
-                div.html(now + "<br/>PowerOff<br/>" + start + "<br/>" + end)
-                    .style("visibility", "visible")
-                    .style("top", (event.pageY) - 60 + "px")
-                    .style("left", (event.pageX) - 60 + "px")
-            }
-        )
-        .on('mouseout', () => {
-                div.style("visibility", "hidden")
-            }
-        )
 }
--- timeline.go
+++ timeline.go
@@ -16,7 +16,6 @@ type TimelineOutput struct {
 	Result         string
 	ProductionData []TimelineData
 	DowntimeData   []TimelineData
-	PowerOffData   []TimelineData
 }
 
 type TimelineData struct {
@@ -40,46 +39,25 @@ func getTimeLineData(writer http.ResponseWriter, request *http.Request, params h
 	endTime := time.Unix(data.Date/1000, 0)
 	var productionData []TimelineData
 	var downtimeData []TimelineData
-	var powerOffData []TimelineData
 	initialTime := endTime.Add(-24 * time.Hour)
-	previousState := -1
+	productionValue := 1
+	downtimeValue := 0
 	for initialTime.Before(endTime) {
-		randomState := rand.Intn(3-0) + 0
+		productionData = append(productionData, TimelineData{Date: initialTime.Unix(), Value: productionValue})
+		downtimeData = append(downtimeData, TimelineData{Date: initialTime.Unix(), Value: downtimeValue})
 		randomDuration := rand.Intn(61-1) + 1
-		if previousState != randomState {
-			switch randomState {
-			case 0:
-				{
-					productionData = append(productionData, TimelineData{Date: initialTime.Unix(), Value: 1})
-					downtimeData = append(downtimeData, TimelineData{Date: initialTime.Unix(), Value: 0})
-					powerOffData = append(powerOffData, TimelineData{Date: initialTime.Unix(), Value: 0})
-				}
-			case 1:
-				{
-					productionData = append(productionData, TimelineData{Date: initialTime.Unix(), Value: 0})
-					downtimeData = append(downtimeData, TimelineData{Date: initialTime.Unix(), Value: 1})
-					powerOffData = append(powerOffData, TimelineData{Date: initialTime.Unix(), Value: 0})
-				}
-			case 2:
-				{
-					productionData = append(productionData, TimelineData{Date: initialTime.Unix(), Value: 0})
-					downtimeData = append(downtimeData, TimelineData{Date: initialTime.Unix(), Value: 0})
-					powerOffData = append(powerOffData, TimelineData{Date: initialTime.Unix(), Value: 1})
-				}
-			}
-		}
-		previousState = randomState
 		initialTime = initialTime.Add(time.Duration(randomDuration) * time.Minute)
+		tempValue := productionValue
+		productionValue = downtimeValue
+		downtimeValue = tempValue
 	}
-	productionData = append(productionData, TimelineData{Date: endTime.Unix(), Value: 0})
-	downtimeData = append(downtimeData, TimelineData{Date: endTime.Unix(), Value: 0})
-	powerOffData = append(powerOffData, TimelineData{Date: endTime.Unix(), Value: 0})
+
 	var responseData TimelineOutput
 	responseData.Result = "ok"
 	responseData.ProductionData = productionData
 	responseData.DowntimeData = downtimeData
-	responseData.PowerOffData = powerOffData
+
 	writer.Header().Set("Content-Type", "application/json")
 	_ = json.NewEncoder(writer).Encode(responseData)
-	logInfo("MAIN", "Parsing data ended")
+	logInfo("MAIN", "Parsing data ended successfully")
 }
